6e75697b66dd1b034bcb0e9a7d6533d8daafdf3c
Audric Ganser <aganser@wireless-10-147-176-57.public.utexas.edu>
2017-03-14 14:04:11 -0500

    Initial Commit

---
+++ PantryFi/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PantryFi
+//
+//  Created by Audric Ganser on 3/14/17.
+//  Copyright © 2017 IOS Group 5. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
