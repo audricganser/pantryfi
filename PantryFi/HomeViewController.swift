875d8cadabc9aca9461ff855e3786a7cebbd5d3d
Hassaan Raza <rhrh1997@gmail.com>
2017-03-23 00:56:32 -0500

    TableView for Ingredients setup with CoreData
    UITableView with CoreData as the storage, will be changed to Firebase
    at a later date

--- PantryFi/HomeViewController.swift
+++ PantryFi/HomeViewController.swift
@@ -7,13 +7,25 @@
 //
 
 import UIKit
+import CoreData
 
-class HomeViewController: UIViewController {
 
+
+class HomeViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    var ingredients = [NSManagedObject]()
+
+    @IBOutlet var tableView: UITableView!
+    
+    let addCellIdentifier = "addCell"
+    let itemIdentifier = "itemCell"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        self.navigationItem.title = "PantriFi"
+        tableView.delegate = self
+        tableView.dataSource = self
+        loadData()
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,6 +33,129 @@ class HomeViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        if(ingredients.count == 0)
+        {
+            return 1
+        }
+        else
+        {
+            return ingredients.count + 1
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if(indexPath.row == 0)
+        {
+            let addCell = tableView.dequeueReusableCell(withIdentifier: addCellIdentifier) as! AddIngredientTableViewCell
+            return addCell
+        }
+        else
+        {
+            let itemCell = tableView.dequeueReusableCell(withIdentifier: itemIdentifier) as! IngredientTableViewCell
+            let row = indexPath.row
+            let ingredient = ingredients[row-1]
+            itemCell.textLabel?.text = ingredient.value(forKey: "name") as? String
+            itemCell.quantityLabel.text = ingredient.value(forKey: "quantity") as? String
+
+            return itemCell
+
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated:true)
+        
+        let row = indexPath.row
+        if(row == 0)
+        {
+            didTapAddItem()
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        
+            return 60
+    }
+    
+    func didTapAddItem()
+    {
+        let alert = UIAlertController(title: "New Ingredient", message:"Insert name of item and quantity", preferredStyle: .alert)
+        alert.addTextField(configurationHandler: nil)
+        alert.addTextField(configurationHandler: nil)
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: { (_) in
+            if let title = alert.textFields?[0].text
+            {
+                if let itemQuantity = alert.textFields?[1].text
+                {
+                    self.saveIngredient(name: title, quantity: itemQuantity)
+                }
+            }
+        }))
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+    fileprivate func saveIngredient(name: String, quantity: String) {
+        
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        
+        let managedContext = appDelegate.persistentContainer.viewContext
+        
+        // Create the entity we want to save
+        let entity =  NSEntityDescription.entity(forEntityName: "Ingredient", in: managedContext)
+        
+        let ingredient = NSManagedObject(entity: entity!, insertInto: managedContext)
+        
+        // Set the attribute values
+        ingredient.setValue(name, forKey: "name")
+        ingredient.setValue(quantity, forKey: "quantity")
+        
+        
+        // Commit the changes.
+        do {
+            try managedContext.save()
+        } catch {
+            // what to do if an error occurs?
+            let nserror = error as NSError
+            print("Unresolved error \(nserror), \(nserror.userInfo)")
+            abort()
+        }
+        
+        // Add the new entity to our array of managed objects
+        ingredients.append(ingredient)
+        tableView.reloadData()
+    }
+    
+    fileprivate func loadData() {
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        
+        let managedContext = appDelegate.persistentContainer.viewContext
+        
+        let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName:"Ingredient")
+        
+        var fetchedResults:[NSManagedObject]? = nil
+        
+        do {
+            try fetchedResults = managedContext.fetch(fetchRequest) as? [NSManagedObject]
+        } catch {
+            // what to do if an error occurs?
+            let nserror = error as NSError
+            NSLog("Unresolved error \(nserror), \(nserror.userInfo)")
+            abort()
+        }
+        
+        if let results = fetchedResults {
+            ingredients = results
+        } else {
+            print("Could not fetch")
+        }
+    }
+
 
     /*
     // MARK: - Navigation
---
+++ PantryFi/IngredientTableViewCell.swift
@@ -0,0 +1,25 @@
+//
+//  IngredientTableViewCell.swift
+//  PantryFi
+//
+//  Created by RAZA on 3/22/17.
+//  Copyright © 2017 IOS Group 5. All rights reserved.
+//
+
+import UIKit
+
+class IngredientTableViewCell: UITableViewCell {
+
+    @IBOutlet var quantityLabel: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
