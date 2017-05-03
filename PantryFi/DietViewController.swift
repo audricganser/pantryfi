54c32b25a32a18125f67510a89bfc2c29ad74b9a
Audric Ganser <Audric Ganser>
2017-05-03 17:48:22 -0500

    working on allergies

--- PantryFi/DietViewController.swift
+++ PantryFi/DietViewController.swift
@@ -7,19 +7,49 @@
 //
 
 import UIKit
+import FirebaseDatabase
+import FirebaseAuth
+
 
 class DietViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, UISearchBarDelegate  {
+    
+    var items = [Diet]()
+    var ref = FIRDatabase.database().reference(withPath: "dietList")
 
-    //var allergies = ["Milk", "Eggs", "Fish", "Crustacean shellfish", "Tree Nuts", "Peanuts", "Wheat", "Soybeans", "Gluten", "Soy", "Sulfite"]
     @IBOutlet weak var tableView: UITableView!
     
-    
-    var diets = ["Vegan", "Vegetarian", "Gluten Free", "Ketogenic", "Whole 30", "Paleo"]
+    var diets:[Diet] = [Diet.init(diet: "Vegan", dietSwitch: false), Diet.init(diet: "Vegetarian", dietSwitch: false), Diet.init(diet: "Gluten Free", dietSwitch: false), Diet.init(diet: "Ketogenic", dietSwitch: false), Diet.init(diet: "Whole 30", dietSwitch: false), Diet.init(diet: "Paleo", dietSwitch: false)]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         tableView.tableFooterView = UIView()
+        
+        if let user = FIRAuth.auth()?.currentUser
+        {
+            let uid = user.uid
+            
+            ref.child(uid).queryOrdered(byChild: "completed").observe(.value, with: { snapshot in
+                var newItems: [Diet] = []
+                
+                for item in snapshot.children {
+                    let dietItem = Diet(snapshot: item as! FIRDataSnapshot)
+                    newItems.append(dietItem)
+                }
+                self.items = self.diets
+                
+                self.tableView.reloadData()
+            })
+        }
+        
+        if self.items.count == 0 {
+            self.items = self.diets
+            print("items here \(self.items)")
+            for d in self.diets {
+                self.saveDiet(diet: d)
+            }
+        }
+
         
         tableView.isScrollEnabled = false;
         tableView.delegate = self
@@ -39,6 +69,7 @@ class DietViewController: UIViewController, UITableViewDataSource, UITableViewDe
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
+        print("diets count \(diets.count)")
         return self.diets.count
     }
     
@@ -49,31 +80,49 @@ class DietViewController: UIViewController, UITableViewDataSource, UITableViewDe
         DietTableViewCell
         
         cell.selectionStyle = UITableViewCellSelectionStyle.none
-        
-        cell.dietLabel.text = diets[indexPath.row]
+        print("items \(self.items)")
+        print("index path row \(indexPath.row)")
+        cell.dietLabel.text = self.items[indexPath.row].diet
+        cell.dietCellSwitch.tag = indexPath.row
         
         return cell
     }
     
-    //    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-    //        tableView.deselectRow(at: indexPath, animated:true)
-    //
-    //        let row = indexPath.row
-    //        if(row == 0)
-    //        {
-    //            didTapAddItem()
-    //        }
-    //    }
+    func saveDiet (diet:Diet) {
+        // save  into data base
+        if let user = FIRAuth.auth()?.currentUser
+        {
+            let uid = user.uid
+            let diet = Diet(diet:diet.diet, dietSwitch:diet.dietSwitch)
+        
+            let dietItemRef = self.ref.child(uid).child((title?.lowercased())!)
+        
+            dietItemRef.setValue(diet.toAnyObject())
+        }
+
+        
+    }
     
-    
-    /*
-     // MARK: - Navigation
-     
-     // In a storyboard-based application, you will often want to do a little preparation before navigation
-     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-     // Get the new view controller using segue.destinationViewController.
-     // Pass the selected object to the new view controller.
-     }
-     */
-    
+    @IBAction func switchChange(_ sender: UISwitch) {
+        print("switch changed")
+        let row = sender.tag
+        let diet = items[row]
+        diet.dietSwitch = sender.isOn
+        saveDiet(diet: diet)
+        
+        //var switchResult =
+//        let title = "placeholder"
+//        if self.dietSwitch.isOn {
+//            dietSwitch = self.dietSwitch.isOn
+//        }
+//        if let user = FIRAuth.auth()?.currentUser
+//        {
+//            let uid = user.uid
+//            let diet = Diet(name:title, dietSwitch:amount)
+//            
+//            let dietItemRef = self.ref.child(uid).child((title?.lowercased())!)
+//            
+//            dietItemRef.setValue(diet.toAnyObject())
+//        }
+    }
 }
