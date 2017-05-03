d9a00c3048c76b15263944bec2cdc02a18d5fe6e
Hassaan Raza <rhrh1997@gmail.com>
2017-05-03 14:57:22 -0500

    Implemented Favorites UICollectionView

--- PantryFi/FavoritesCollectionViewController.swift
+++ PantryFi/FavoritesCollectionViewController.swift
@@ -7,21 +7,54 @@
 //
 
 import UIKit
+import FirebaseAuth
+import FirebaseDatabase
+import Alamofire
 
 private let reuseIdentifier = "Cell"
+
+
+let ref = FIRDatabase.database().reference(withPath: "recipes")
+
+var items = [RecipeWithIngredients]()
+var images = [UIImage]()
 
 class FavoritesCollectionViewController: UICollectionViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Uncomment the following line to preserve selection between presentations
+        
         // self.clearsSelectionOnViewWillAppear = false
 
         // Register cell classes
-        self.collectionView!.register(UICollectionViewCell.self, forCellWithReuseIdentifier: reuseIdentifier)
 
-        // Do any additional setup after loading the view.
+        collectionView?.delegate = self
+        collectionView?.dataSource = self
+        
+        // 1
+        if let user = FIRAuth.auth()?.currentUser
+        {
+            let uid = user.uid
+            ref.child(uid).observe(.value, with: { snapshot in
+                // 2
+                var newItems: [RecipeWithIngredients] = []
+                
+                // 3
+                for item in snapshot.children {
+                    // 4
+                    let recipeItem = RecipeWithIngredients(snapshot: item as! FIRDataSnapshot)
+                    newItems.append(recipeItem)
+                }
+                
+                // 5
+                items = newItems
+                self.collectionView?.reloadData()
+            })
+        }
+
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -43,20 +76,31 @@ class FavoritesCollectionViewController: UICollectionViewController {
 
     override func numberOfSections(in collectionView: UICollectionView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
 
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of items
-        return 0
+        return items.count
     }
 
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath)
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "favoriteCollection", for: indexPath) as! FavoriteCollectionViewCell
     
-        // Configure the cell
     
+        let row = indexPath.row
+        let recipe = items[row]
+        Alamofire.request(recipe.image).response { response in
+            if let data = response.data {
+                let image = UIImage(data: data)
+                cell.imageView.image = image
+
+            } else {
+                print("Data is nil. I don't know what to do :(")
+            }
+        }
+        cell.titleLabel.text = recipe.title
         return cell
     }
 
@@ -69,12 +113,16 @@ class FavoritesCollectionViewController: UICollectionViewController {
     }
     */
 
-    /*
-    // Uncomment this method to specify if the specified item should be selected
+
     override func collectionView(_ collectionView: UICollectionView, shouldSelectItemAt indexPath: IndexPath) -> Bool {
         return true
     }
-    */
+
+    override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let row = indexPath.row
+        let id = items[row].id
+        //Open storyboard
+    }
 
     /*
     // Uncomment these methods to specify if an action menu should be displayed for the specified item, and react to actions performed on the item
